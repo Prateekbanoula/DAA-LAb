21a10eb4fcb6ea8ece822ea81f04e98c93dc913c
Prateekbanoula <prateekbanoula65@gmail.com>
2026-01-24 12:53:39 +0530

    Implemented Linear Search with comparison count

--- Week-1/LINEAR_SEARCH.py
+++ Week-1/LINEAR_SEARCH.py
@@ -1,7 +1,7 @@
 '''Question:- Given an array of nonnegative integers, design a linear algorithm
               and implement it using a program to find whether given key element is
               present in the array or not. Also, find total number of comparisons for each input case. 
-              (Time Complexity = O(n), where n is the size of input)'''
+             (Time Complexity = O(n), where n is the size of input)'''
 
 def linear_search(arr,key):
     comparisons = 0
